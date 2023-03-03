1a74a48c7e622116733ef57730cb294b6b411762
vramaiah <vijvalvramaiah@gmail.com>
2023-03-02 20:11:02 -0500

    Update redirect to lobby to be more efficient

--- about/views.py
+++ about/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render, redirect
 
 def index(request):
     """Home page for our site"""
-    if request.user.is_authenticated:
-        return redirect('game:lobby')
-    else:
-        return render(request, 'about/index.html')
+    return render(request, 'about/index.html')
